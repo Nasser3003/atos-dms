0474fcf744fc44f1be5b696c1def4ef0643069bc
Abdrabbo MBP 14 M1 <abdo.abdo3003@gmail.com>
2024-08-12 21:54:23 +0300

    initial commit

---
+++ auth-service/src/main/java/net/atos/entity/UserInformation.java
@@ -0,0 +1,23 @@
+package net.atos.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class UserInformation {
+
+    @Id
+    @Setter(AccessLevel.NONE)
+    private long userId;
+
+    private String username;
+    private String password;
+    private String description;
+
+
+}
