6fedb80d98f665b73e84bfdc08632c141bcd47d7
Abdrabbo MBP 14 M1 <abdo.abdo3003@gmail.com>
2024-09-04 00:44:12 +0300

    added WebMvcConfigurer to allow cross origin from frontend

--- auth-user-service/src/main/java/net/atos/controller/AuthController.java
+++ auth-user-service/src/main/java/net/atos/controller/AuthController.java
@@ -6,13 +6,11 @@ import net.atos.dto.LoginResponseDTO;
 import net.atos.service.UserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AuthController {
     private final UserAuthService userAuthService;
